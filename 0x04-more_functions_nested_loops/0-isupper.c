cd7820f5319441dcfefdf9c3f71decd6edc31818
odunuga-kehinde <odunugakehinde92@gmail.com>
2023-03-23 15:56:12 -0700

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,15 @@
 
 /**
  * _isupper -  Write a function that checks for uppercase character
+ * @c: character to check
  *
- * Return: 1 if true. Else, return 0
+ * Return: 0 or 1
  */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
 }
