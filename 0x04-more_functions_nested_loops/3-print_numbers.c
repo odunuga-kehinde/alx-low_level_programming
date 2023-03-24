4e5f36aad89891c92d5a6727d60648bcc2d405c9
odunuga-kehinde <odunugakehinde92@gmail.com>
2023-03-24 03:45:29 -0700

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-* owned by Bwave ICT
-*Return: void
-*/
+ * print_numbers - print 0123456789
+ * 
+ * Return: void
+ */
 
 void print_numbers(void)
 {
