b9236c37400a7d8bcc313de931ebd67b33dd7ddf
odunuga-kehinde <odunugakehinde92@gmail.com>
2023-03-27 14:00:28 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: string
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i, j;
+
+	j = 0;
+
+	while (s[j] != '\0')
+		j++;
+
+	for (i = j - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
